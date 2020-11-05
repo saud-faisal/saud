1371e45af43cfbd1553c1c7b284e80e3eb3eb9bf
saud-faisal <58050211+saud-faisal@users.noreply.github.com>
2020-11-05 12:08:56 +0530

    Add files via upload

---
+++ ds.py
@@ -0,0 +1,44 @@
+
+#from bs4 import BeautifulSoup
+"""<html>
+<head>
+<title>demo site</title>
+</head>
+<body>
+<h1>Welcome</h1>
+<div>
+ <p>this is para</p>
+ <p>this is another para</p>
+ <span>this is span</span>
+ <a href="hi" class="hehe">hello</a>
+</div>
+</body>
+</html>
+"""
+#soup=BeautifulSoup(doc,'html.parser')
+#print(soup.prettify())
+#print(soup.title)
+#print(soup.find_all("p"))
+#for x in soup.a.contents:
+#	print(x)
+#	x.pop()
+#class room:
+#	x=20
+#	y=30
+#	def fun(self):
+#		return "add"
+def add(x,y):
+	return x+y
+	
+def subtract(x,y):
+	return x-y
+
+def multiply (x,y):
+	return x*y
+	
+def divide(x,y):
+	return x/y
+
+
+
+
