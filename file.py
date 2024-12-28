e3b7e9c4dcaf532d5a358bc089ffaef3f04e8d78
saud faisal <saud.faizal@pinogy.com>
2024-12-28 20:53:19 +0530

    add in gitignore and update file

--- file.py
+++ file.py
@@ -1,2 +1,8 @@
-print("hello world")
-print("kjsflijlkejfsj e")
+class demo:
+    def __init__(self):
+        pass
+    def call_me(self):
+        return 'call'
+    def deny_call(self):
+        return "deny call"
+d = demo()
